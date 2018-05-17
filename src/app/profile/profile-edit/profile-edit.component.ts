fe54741b411c11056159284a274f21ffd9a3db87
alex dubuisson <adubu002@fiu.edu>
2018-05-17 16:29:42 -0400

    activated edit profile link

--- src/app/profile/profile-edit/profile-edit.component.ts
+++ src/app/profile/profile-edit/profile-edit.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.component.html',
-  styleUrls: ['./profile-edit.component.css']
+  styleUrls: ['./profile-edit.component.scss']
 })
 export class ProfileEditComponent implements OnInit {
 
