4d23b4b725e06aa3baa530487126fb610a1eda7d
alex dubuisson <adubu002@fiu.edu>
2018-05-16 22:53:17 -0400

    modified profile dashboard

--- src/app/profile/profile-dashboard/profile-dashboard.component.ts
+++ src/app/profile/profile-dashboard/profile-dashboard.component.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/Rx';
 @Component({
   selector: 'app-profile-dashboard',
   templateUrl: './profile-dashboard.component.html',
-  styleUrls: ['./profile-dashboard.component.css']
+  styleUrls: ['./profile-dashboard.component.scss']
 })
 export class ProfileDashboardComponent implements OnInit {
 
