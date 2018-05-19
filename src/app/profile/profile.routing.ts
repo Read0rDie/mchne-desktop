b93e5cb9c61293233c7ac693de2b1934b95c8636
Alex Dubuisson <adydd11@gmail.com>
2018-05-19 00:53:14 -0400

    linked delete profile and change password pages to teh profile dashboard

--- src/app/profile/profile.routing.ts
+++ src/app/profile/profile.routing.ts
@@ -4,9 +4,13 @@ import { RouterModule }        from '@angular/router';
 import { AvatarSelectionComponent }    from './avatar-selection/avatar-selection.component';
 import { ProfileDashboardComponent }    from './profile-dashboard/profile-dashboard.component';
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { PasswordEditComponent } from './password-edit/password-edit.component';
+import { ProfileDeleteComponent } from './profile-delete/profile-delete.component';
 
 export const routing: ModuleWithProviders = RouterModule.forChild([
   { path: 'profile/avatars', component: AvatarSelectionComponent},
   { path: 'profile', component: ProfileDashboardComponent},
-  { path: 'profile/edit', component: ProfileEditComponent}
+  { path: 'profile/edit', component: ProfileEditComponent},
+  { path: 'profile/password', component: PasswordEditComponent},
+  { path: 'profile/delete', component: ProfileDeleteComponent}
 ]);
