6999bc4c4c3e04ac58b0f4901df725d47a29a54a
Alex Dubuisson <adydd11@gmail.com>
2018-05-27 02:04:13 -0400

    fixed linebreak pipe to avoid cutting a string mid-word

--- src/app/shared/pipes/line-break.pipe.ts
+++ src/app/shared/pipes/line-break.pipe.ts
@@ -1,9 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const isAlphaNumeric = ch => {
+	return ch.match(/^[a-z0-9]+$/i) !== null;
+}
+
 @Pipe({name: 'lineBreak'})
+
 
 export class LineBreakPipe implements PipeTransform {
   transform(text: string): string {
-    return text.replace(/(\\r\\n)|([\r\n])/gmi, '<br/>');
+    var index = 300;
+
+    var trim = text.substr(0, index);
+    trim = trim.substr(0, Math.min(trim.length, trim.lastIndexOf(" ")))
+
+    return trim;    
   }
 }
